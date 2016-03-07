d549ef815a06a1ddd1e351a146a826d93eae1335
Dreanor <mikeundead@googlemail.com>
2016-03-07 19:58:55 +0100

    fixed 403 error

--- NekoBot/Services/MessageService.cs
+++ NekoBot/Services/MessageService.cs
@@ -4,6 +4,7 @@ using DiscordSharp.Objects;
 using NekoBot.Commands;
 using System;
 using System.Linq;
+using System.Threading;
 
 namespace NekoBot.Services
 {
@@ -13,6 +14,7 @@ namespace NekoBot.Services
         private Config config;
         private AdminCommands adminCommands;
         private UserCommands userCommands;
+        private Semaphore apiCallsBlocker;
 
         public MessageService(DiscordClient client, Config config)
         {
@@ -20,32 +22,45 @@ namespace NekoBot.Services
             this.client = client;
             adminCommands = new AdminCommands(client);
             userCommands = new UserCommands();
+            apiCallsBlocker = new Semaphore(0, 5);
         }
 
         public void PrivateMessageReceived(object sender, DiscordPrivateMessageEventArgs e)
         {
+            apiCallsBlocker.WaitOne();
             e.Channel.recipient.SendMessage("Sorry! Private message commands don't work. Please talk to me in a public channel.");
+            apiCallsBlocker.Release(1);
         }
 
         public void MessageReceived(object sender, DiscordMessageEventArgs e)
         {
-            DiscordChannel channel = e.Channel;
-            string message = e.message_text;
-
-            //todo limit to x amount in minute
-            if (!AllowedToListenTo(channel))
+            try
             {
-                return;
+                DiscordChannel channel = e.Channel;
+                string message = e.message_text;
+
+                if (!AllowedToListenTo(channel))
+                {
+                    return;
+                }
+
+                Console.WriteLine(e.message_text);
+
+                apiCallsBlocker.WaitOne();
+                if (AllowedToExecuteAdminCommands(e))
+                {
+                    adminCommands.HandleCommands(e);
+                }
+
+                if (userCommands.Commands.Any(x => message.StartsWith(x)))
+                {
+                    userCommands.HandleCommands(message, channel);
+                }
+                apiCallsBlocker.Release(1);
             }
-
-            if (AllowedToExecuteAdminCommands(e))
+            catch (Exception ex)
             {
-                adminCommands.HandleCommands(e);
-            }
-
-            if (userCommands.Commands.Any(x => message.StartsWith(x)))
-            {
-                userCommands.HandleCommands(message, channel);
+                Console.WriteLine(string.Format("Latest message send: '{0}' by '{1}'{2}{3}", e.message_text, e.author, Environment.NewLine, ex.Message));
             }
         }
 
